8ba02b77124ef09e231970c06e1be201558fbba9
d4an <angeldaniel6709@gmail.com>
2023-02-16 16:14:42 -0500

    VentanaV.2.0.1

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -1,10 +1,13 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainGui extends JFrame {
     public MainGui() {
         super("Documento sin titulo");
+        Image icon = Toolkit.getDefaultToolkit().getImage("src\\img\\logoUGwritter.png");
+        setIconImage(icon);
         setSize(800, 600);
         //hacer que el tamaño de la ventana ocupe toda la pantalla pero que se pueda hacer mas pequeña
         setExtendedState(JFrame.MAXIMIZED_BOTH);
